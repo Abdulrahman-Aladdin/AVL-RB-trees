5da667012fa8296b8a0e6ce39a9eba45e8944d2c
MohamedAnwar121 <mohamedabdalrahman6@gmail.com>
2023-03-30 00:12:14 +0200

    batch insert and delete tests added

--- src/test/java/_Dictionary/DictionaryTest.java
+++ src/test/java/_Dictionary/DictionaryTest.java
@@ -22,11 +22,23 @@ class DictionaryTest {
         Assertions.assertTrue(dictionary.insert("abdo alaa"));
         Assertions.assertTrue(dictionary.insert("mohamed arous"));
 
+        // check size
+        Assertions.assertEquals(5,dictionary.size());
+
+        // check height
+        Assertions.assertEquals(3,dictionary.treeHeight());
+
         // search for a string in dictionary
         Assertions.assertTrue(dictionary.search("mohamed elhabiby"));
 
         // delete string in dictionary
         Assertions.assertTrue(dictionary.delete("hussein mohamed"));
+
+        // check size
+        Assertions.assertEquals(4,dictionary.size());
+
+        // check height
+        Assertions.assertEquals(3,dictionary.treeHeight());
 
         // search for a string deleted form dictionary
         Assertions.assertFalse(dictionary.search("hussein mohamed"));
@@ -37,8 +49,20 @@ class DictionaryTest {
         // delete a string not in dictionary
         Assertions.assertFalse(dictionary.delete("hussein khaled"));
 
+        // check size
+        Assertions.assertEquals(4,dictionary.size());
+
+        // check height
+        Assertions.assertEquals(3,dictionary.treeHeight());
+
         // insert a string already in dictionary
         Assertions.assertFalse(dictionary.insert("mohamed anwar"));
+
+        // check size
+        Assertions.assertEquals(4,dictionary.size());
+
+        // check height
+        Assertions.assertEquals(3,dictionary.treeHeight());
     }
 
     @Test
@@ -52,11 +76,23 @@ class DictionaryTest {
         Assertions.assertTrue(dictionary.insert("abdo alaa"));
         Assertions.assertTrue(dictionary.insert("mohamed arous"));
 
+        // check size
+        Assertions.assertEquals(5,dictionary.size());
+
+        // check height
+        Assertions.assertEquals(3,dictionary.treeHeight());
+
         // search for a string in dictionary
         Assertions.assertTrue(dictionary.search("mohamed elhabiby"));
 
         // delete string in dictionary
         Assertions.assertTrue(dictionary.delete("hussein mohamed"));
+
+        // check size
+        Assertions.assertEquals(4,dictionary.size());
+
+        // check height
+        Assertions.assertEquals(3,dictionary.treeHeight());
 
         // search for a string deleted form dictionary
         Assertions.assertFalse(dictionary.search("hussein mohamed"));
@@ -67,8 +103,58 @@ class DictionaryTest {
         // delete a string not in dictionary
         Assertions.assertFalse(dictionary.delete("hussein khaled"));
 
+        // check size
+        Assertions.assertEquals(4,dictionary.size());
+
+        // check height
+        Assertions.assertEquals(3,dictionary.treeHeight());
+
         // insert a string already in dictionary
         Assertions.assertFalse(dictionary.insert("mohamed anwar"));
+
+        // check size
+        Assertions.assertEquals(4,dictionary.size());
+
+        // check height
+        Assertions.assertEquals(3,dictionary.treeHeight());
+    }
+
+    @Test
+    void testBatchAVL(){
+        dictionary = DictionaryFactory.getDictionary(Tree.AVL);
+
+        assertEquals(0,dictionary.size());
+
+        var insertResult = dictionary.batchInsert("testBatchInsert.txt");
+        assertEquals(insertResult.getLeft(),10);
+        assertEquals(insertResult.getRight(),3);
+
+        assertEquals(10,dictionary.size());
+
+        var deleteResult = dictionary.batchDelete("testBatchDelete.txt");
+        assertEquals(deleteResult.getLeft(),4);
+        assertEquals(deleteResult.getRight(),2);
+
+        assertEquals(6,dictionary.size());
+    }
+
+    @Test
+    void testBatchRedBlack(){
+        dictionary = DictionaryFactory.getDictionary(Tree.RED_BLACK);
+
+        assertEquals(0,dictionary.size());
+
+        var insertResult = dictionary.batchInsert("testBatchInsert.txt");
+        assertEquals(insertResult.getLeft(),10);
+        assertEquals(insertResult.getRight(),3);
+
+        assertEquals(10,dictionary.size());
+
+        var deleteResult = dictionary.batchDelete("testBatchDelete.txt");
+        assertEquals(deleteResult.getLeft(),4);
+        assertEquals(deleteResult.getRight(),2);
+
+        assertEquals(6,dictionary.size());
     }
 
 }
